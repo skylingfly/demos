6379a8df5ce74c63aee81c1de68e9c2f8a72ebc0
skyling <libing.shi@ishanggang.com>
2018-11-12 10:36:35 +0800

    init

---
+++ src/components/Title.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class='title'>
+    <p>{{title}}</p>
+    <p>{{subtitle}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['title', 'subtitle']
+}
+</script>
+<style scoped>
+  .title {
+    color: 'red';
+  }
+
+</style>
